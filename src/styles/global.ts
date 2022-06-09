f1845aaae326571b8443099044c91d4a36b6bdf0
root <root@DESKTOP-3REDR5J.localdomain>
2022-06-09 16:18:04 -0400

    chore: estilização modal finalizada

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
     --text-body: #969cb3;
     --shape: #ffffff;
   }
-  
+
   * {
     margin: 0;
     padding: 0;
@@ -20,17 +20,17 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     @media (min-width: 1080px) {
-      font-size: 93.75%;      
+      font-size: 93.75%;
     }
 
     @media (min-width: 720px) {
-      font-size: 87.5%; 
+      font-size: 87.5%;
     }
   }
 
-  body { 
+  body {
     background: var(--background);
-    -webkit-font-smoothing: antialiased;    
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
@@ -49,6 +49,44 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay{
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
 
 `;
